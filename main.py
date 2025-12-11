82ad21f5944b5e129903353a030300ed5273456d
arti <arti@privategatevpn.com>
2025-12-11 19:53:53 +0300

    Cleared up an example

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ def angled(rad : float):
 
 def frame():
     w.clear()
-    #p1.rotate((1,2),-0.05)
-    p1.rotate((0,2),-0.05)
+    p1.rotate((0,3),-0.05)
+    p1.rotate((1,2),0.05)
     p1.draw_on(w,None)
 
 even = [angled(pi/4),angled(pi*3/4)]
 w = World([center([4]),center([4])],(500,500),"white",((-2,2),(-2,2)))
-p1 = import_OFF("assets/H4.off")
+p1 = import_OFF("tesseract.off")
 p1.draw_on(w)
 w.mainloop(frame,1/60)
